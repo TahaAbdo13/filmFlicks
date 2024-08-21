3ad397a6900fa112b803a06e39b9d61e75ae14ae
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-22 01:18:17 +0300

    add pageViewBuilder in OnBoardering screen view

--- lib/features/onBoardering_view/presentaion/views/widgets/onboardring_view_body.dart
+++ lib/features/onBoardering_view/presentaion/views/widgets/onboardring_view_body.dart
@@ -1,23 +1,41 @@
-
 import 'package:flutter/material.dart';
 import 'custom_card_on_bordering_screen.dart';
+
 class OnboardringViewBody extends StatelessWidget {
   const OnboardringViewBody({super.key});
   @override
   Widget build(BuildContext context) {
-   
     var hieght = MediaQuery.of(context).size.height;
-    return Stack(
-      children: [
-        Container(
-          height: MediaQuery.of(context).size.height,
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/images/Image.png"),
-                  fit: BoxFit.fill)),
-        ),
-        CustomCardOnBorderingScreen(hieght: hieght)
-      ],
-    );
+    List<Widget> onBoarderingScreen = [
+      Stack(
+        children: [
+          Container(
+            height: hieght,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/Image.png"),
+                    fit: BoxFit.fill)),
+          ),
+          CustomCardOnBorderingScreen(hieght: hieght)
+        ],
+      ),
+      Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/Image.png"),
+                    fit: BoxFit.fill)),
+          ),
+          CustomCardOnBorderingScreen(hieght: hieght)
+        ],
+      ),
+    ];
+    return PageView.builder(
+        itemCount: onBoarderingScreen.length,
+        itemBuilder: (context, i) {
+          return onBoarderingScreen[i];
+        });
   }
 }
