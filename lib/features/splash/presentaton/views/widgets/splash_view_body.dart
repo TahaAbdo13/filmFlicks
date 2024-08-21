bd6150b693c989ac48fb569b82d4b963831bba78
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-21 16:47:03 +0300

    add animation to Splash view

--- lib/features/splash/presentaton/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentaton/views/widgets/splash_view_body.dart
@@ -2,8 +2,31 @@ import 'package:filmflicks/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidinImageAnimation;
+  late Animation<Offset> slidinTextAnimation;
+  @override
+  void initState() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+    slidinImageAnimation =
+        Tween<Offset>(begin: const Offset(10, 0), end: Offset.zero)
+            .animate(animationController);
+    slidinTextAnimation =
+        Tween<Offset>(begin: const Offset(-10, 0), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,18 +35,24 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Align(
           alignment: Alignment.center,
-          child: SvgPicture.asset(
-            "assets/images/appLogo.svg",
-            height: 170,
-            width: 170,
+          child: SlideTransition(
+            position: slidinImageAnimation,
+            child: SvgPicture.asset(
+              "assets/images/appLogo.svg",
+              height: 170,
+              width: 170,
+            ),
           ),
         ),
         const SizedBox(
           height: 16,
         ),
-        const Text(
-          "FILM FLICKS",
-          style: Styles.h1,
+        SlideTransition(
+          position: slidinTextAnimation,
+          child: const Text(
+            "FILM FLICKS",
+            style: Styles.h1,
+          ),
         )
       ],
     );
