8c7c371463d1784cde3e2ab41a6678a8cfd52a91
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-22 17:24:37 +0300

    Create SignUpWithRow

--- lib/features/registeraion/presentation/views/widgets/wellcome_view_body.dart
+++ lib/features/registeraion/presentation/views/widgets/wellcome_view_body.dart
@@ -1,12 +1,12 @@
-
+import 'package:filmflicks/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'elvated_button_and_row_section.dart';
 import 'wellcome_screen_logo_section.dart';
+
 class WellcomeViewBody extends StatelessWidget {
   const WellcomeViewBody({super.key});
   @override
   Widget build(BuildContext context) {
-   
     return const Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -19,7 +19,31 @@ class WellcomeViewBody extends StatelessWidget {
         SizedBox(
           height: 64,
         ),
-          ElvatedButtonAndRowSection(),
+        ElvatedButtonAndRowSection(),
+        SizedBox(
+          height: 32,
+        ),
+        SignUpWithRow()
+      ],
+    );
+  }
+}
+
+class SignUpWithRow extends StatelessWidget {
+  const SignUpWithRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Expanded(child: Divider()),
+        Expanded(
+            child: Text(
+          "  Or Sign up with",
+          style: Styles.h5.copyWith(fontWeight: FontWeight.w500),
+        )),
+        const Expanded(child: Divider()),
       ],
     );
   }
