9ff779ceccd20cadfb41b64c48cb5fd601796d16
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-22 16:38:50 +0300

    create wellcome screen logo section

--- lib/features/registeraion/presentation/views/widgets/wellcome_screen_logo_section.dart
+++ lib/features/registeraion/presentation/views/widgets/wellcome_screen_logo_section.dart
@@ -4,19 +4,22 @@ import 'package:filmflicks/core/utils/widgets/custom_app_logo_widget.dart';
 import 'package:filmflicks/core/utils/widgets/custom_app_name_widget.dart';
 import 'package:flutter/material.dart';
 
-import 'wellcome_screen_logo_section.dart';
-
-class WellcomeViewBody extends StatelessWidget {
-  const WellcomeViewBody({super.key});
+class WellcomeScreenLogoSection extends StatelessWidget {
+  const WellcomeScreenLogoSection({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 55.0, vertical: 55),
-          child: WellcomeScreenLogoSection(),
-        ),
+        const CustomAppLogoWidget(),
+        const CustomAppNameWidget(),
+        Text(
+          "Enter your registered Phone Number to Sign Up",
+          style: Styles.h5.copyWith(color: kGreyColor),
+          textAlign: TextAlign.center,
+        )
       ],
     );
   }
