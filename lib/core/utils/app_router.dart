63de9f9e1215153ae24c310428808bcc884088e9
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-22 15:35:42 +0300

    add wellcom view to app router

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,12 @@
 import 'package:filmflicks/features/onBoardering_view/presentaion/views/onBoardring_view.dart';
+import 'package:filmflicks/features/registeraion/presentation/views/wellcome_view.dart';
+
 import 'package:filmflicks/features/splash/presentaton/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const onBoarderingScreenView = "/OnBoardringScreenView";
+  static const wellcomeView = "/wellcomeview";
 
   static final GoRouter router = GoRouter(routes: [
     GoRoute(
@@ -13,6 +16,10 @@ abstract class AppRouter {
     GoRoute(
       path: onBoarderingScreenView,
       builder: (context, state) => const OnboardringView(),
+    ),
+    GoRoute(
+      path: wellcomeView,
+      builder: (context, state) => const WellcomeView(),
     ),
   ]);
 }
--- lib/features/registeraion/presentation/views/widgets/wellcome_view_body.dart
+++ lib/features/registeraion/presentation/views/widgets/wellcome_view_body.dart
@@ -5,6 +5,8 @@ class WellcomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Scaffold());
+    return Column(children: [
+      
+    ],);
   }
 }
