14bb757b69899a23c587782d964ad7e240218613
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-23 02:40:31 +0300

    edit style file

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -11,7 +11,7 @@ abstract class Styles {
   static const h4 =
       TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: kWhiteColor);
   static const h5 =
-      TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: kWhiteColor);
+      TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: kGreyColor);
   static const h6 =
-      TextStyle(fontSize: 12, fontWeight: FontWeight.w600, color: kWhiteColor);
+      TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: kGreyColor);
 }
