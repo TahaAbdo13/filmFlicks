f0094283cadc0a6d970cae42a900d70ecfdd1853
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-24 18:10:04 +0300

    create sign up page done

--- lib/features/registeraion/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/registeraion/presentation/views/widgets/sign_up_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:filmflicks/core/utils/constants.dart';
 import 'package:filmflicks/core/utils/styles.dart';
 import 'package:filmflicks/features/registeraion/presentation/views/widgets/custom_appBar_widget.dart';
+import 'package:filmflicks/features/registeraion/presentation/views/widgets/custom_elevated_button.dart';
 import 'package:filmflicks/features/registeraion/presentation/views/widgets/custom_text_form_field.dart';
 import 'package:filmflicks/features/registeraion/presentation/views/widgets/view_body_text_section.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,8 @@ class SignUpViewBody extends StatelessWidget {
   const SignUpViewBody({super.key});
   @override
   Widget build(BuildContext context) {
+    var hieght = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24),
       child: Column(
@@ -40,6 +43,11 @@ class SignUpViewBody extends StatelessWidget {
           const CustomTextFormField(label: "Password"),
           const SizedBox(height: 16),
           const CustomPrivacyRow(),
+          const SizedBox(height: 40),
+          SizedBox(
+              width: width,
+              height: hieght * 0.075,
+              child: CustomElevatedButton(onPressed: () {}, text: "Sign Up"))
         ],
       ),
     );
