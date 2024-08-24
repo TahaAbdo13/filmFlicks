1e132dee3f7a4c035895861d9841c7f2e27d3c4c
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-24 18:33:26 +0300

    reset password body done

--- lib/features/registeraion/presentation/views/reset_password_view.dart
+++ lib/features/registeraion/presentation/views/reset_password_view.dart
@@ -1,12 +1,16 @@
+import 'package:filmflicks/core/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'widgets/reset_password_view_body.dart';
+
 class ResetPasswordView extends StatelessWidget {
   const ResetPasswordView({super.key});
   @override
   Widget build(BuildContext context) {
     return const SafeArea(
         child: Scaffold(
+      backgroundColor: kCardColor,
       body: ResetPasswordViewBody(),
+      
     ));
   }
 }
--- lib/features/registeraion/presentation/views/widgets/reset_password_view_body.dart
+++ lib/features/registeraion/presentation/views/widgets/reset_password_view_body.dart
@@ -1,3 +1,8 @@
+import 'package:filmflicks/core/utils/styles.dart';
+import 'package:filmflicks/features/registeraion/presentation/views/widgets/custom_appBar_widget.dart';
+import 'package:filmflicks/features/registeraion/presentation/views/widgets/custom_elevated_button.dart';
+import 'package:filmflicks/features/registeraion/presentation/views/widgets/custom_text_form_field.dart';
+import 'package:filmflicks/features/registeraion/presentation/views/widgets/view_body_text_section.dart';
 import 'package:flutter/material.dart';
 
 class ResetPasswordViewBody extends StatelessWidget {
@@ -5,6 +10,34 @@ class ResetPasswordViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var hieght = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24),
+      child: Column(
+        children: [
+          const CustomAppBarWidget(title: ""),
+          const SizedBox(
+            height: 40,
+          ),
+          const ViewBodyTextSection(
+              title1: "Reset Password",
+              title2: "Recover your account password",
+              style1: Styles.h1,
+              style2: Styles.h5),
+          const SizedBox(
+            height: 48,
+          ),
+          const CustomTextFormField(label: "Email Address"),
+          const SizedBox(
+            height: 40,
+          ),
+          SizedBox(
+              width: width,
+              height: hieght * 0.075,
+              child: CustomElevatedButton(onPressed: () {}, text: "Next"))
+        ],
+      ),
+    );
   }
 }
