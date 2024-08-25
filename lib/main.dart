1bde94202d4dfd6557724bbf2b4109efc4723c53
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-26 01:01:03 +0300

    intialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import "package:filmflicks/core/utils/app_router.dart";
 import "package:filmflicks/core/utils/constants.dart";
+import "package:filmflicks/firebase_options.dart";
+import "package:firebase_core/firebase_core.dart";
 
 import "package:flutter/material.dart";
 import "package:google_fonts/google_fonts.dart";
 
-void main() {
+void main()async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FilmFlicks());
 }
 
