7aabf952cfe054354edbcef6dc07d3dda542add1
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-26 18:34:05 +0300

    create SignIn Cubit State

--- lib/features/registeraion/presentation/manager/sign_in_cubit/sign_in_cubit_state.dart
+++ lib/features/registeraion/presentation/manager/sign_in_cubit/sign_in_cubit_state.dart
@@ -1,6 +1,16 @@
 part of 'sign_in_cubit_cubit.dart';
 
 @immutable
-sealed class SignInCubitState {}
+abstract class SignInCubitState {}
 
 final class SignInCubitInitial extends SignInCubitState {}
+
+final class SignInCubitSuccess extends SignInCubitState {}
+
+final class SignInCubitLoading extends SignInCubitState {}
+
+final class SignInCubitFailure extends SignInCubitState {
+  final String errMessage;
+
+  SignInCubitFailure({required this.errMessage});
+}
