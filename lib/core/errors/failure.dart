433c59009b10da8ac43134e97c029baa1a019c3d
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-29 15:14:49 +0300

    add check log in error to Auth Error

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -4,19 +4,22 @@ abstract class Failure {
   final String errMessage;
 
   Failure(this.errMessage);
-
 }
-class AuthError extends Failure{
+
+class AuthError extends Failure {
   AuthError(super.errMessage);
-  factory AuthError.firebaseAuhtExeption(FirebaseAuthException firebaseAuthExeption){
+  factory AuthError.firebaseAuhtExeption(
+      FirebaseAuthException firebaseAuthExeption) {
     if (firebaseAuthExeption.code == 'weak-password') {
-  return AuthError('The password provided is too weak.');
-  } else if (firebaseAuthExeption.code == 'email-already-in-use') {
-    return AuthError('The account already exists for that email.');
-  }else{
-    return AuthError(firebaseAuthExeption.message!);
+      return AuthError('The password provided is too weak.');
+    } else if (firebaseAuthExeption.code == 'email-already-in-use') {
+      return AuthError('The account already exists for that email.');
+    } else if (firebaseAuthExeption.code == 'user-not-found') {
+      return AuthError("No user found for that email.");
+    } else if (firebaseAuthExeption.code == 'wrong-password') {
+      return AuthError('Wrong password provided for that user.');
+    } else {
+      return AuthError(firebaseAuthExeption.message!);
+    }
   }
-  }
-
 }
-
