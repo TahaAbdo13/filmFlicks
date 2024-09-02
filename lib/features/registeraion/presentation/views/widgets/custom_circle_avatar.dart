be9ec8fa61a43476bf8f5b11bef5d225ffa9a6a1
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-09-02 16:39:43 +0300

    integrate Wellcome sign in with google cubit

--- lib/features/registeraion/presentation/views/widgets/custom_circle_avatar.dart
+++ lib/features/registeraion/presentation/views/widgets/custom_circle_avatar.dart
@@ -2,23 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomCircleAvatar extends StatelessWidget {
- final void Function() onTap;
+  final bool isLoading;
+  final void Function() onTap;
   final Color color;
   final String circleImage;
   const CustomCircleAvatar({
     super.key,
     required this.color,
-    required this.circleImage, required this.onTap,
+    required this.circleImage,
+    required this.onTap, required this.isLoading,
   });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap:onTap,
+      onTap: onTap,
       child: CircleAvatar(
           backgroundColor: color,
           radius: 35,
-          child: SvgPicture.asset(circleImage)),
+          child:isLoading?const CircularProgressIndicator(): SvgPicture.asset(circleImage)),
     );
   }
 }
--- lib/features/registeraion/presentation/views/widgets/sign_up_wih_icon_row.dart
+++ lib/features/registeraion/presentation/views/widgets/sign_up_wih_icon_row.dart
@@ -1,25 +1,48 @@
 import 'package:filmflicks/core/utils/constants.dart';
+import 'package:filmflicks/core/utils/functions/show_snack_bar.dart';
+import 'package:filmflicks/features/registeraion/presentation/manager/wellcome_sigIn_google/wellcome_sign_in_google_cubit.dart';
 import 'package:filmflicks/features/registeraion/presentation/views/widgets/custom_circle_avatar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpWihIconRow extends StatelessWidget {
   const SignUpWihIconRow({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-       CustomCircleAvatar(
-        color: kWhiteColor,
-        circleImage: 'assets/images/Icon - Google.svg', onTap: () {  },
-      ),
-       CustomCircleAvatar(
-        color: kSoft,
-        circleImage: 'assets/images/Icon - Apple.svg', onTap: () {  },
-      ),
-      CustomCircleAvatar(
-        color: Colors.blue.withOpacity(0.6),
-        circleImage: 'assets/images/Icon - Facebook.svg', onTap: () {  },
-      ),
-    ]);
+    return BlocConsumer<WellcomeSignInGoogleCubit, WellcomeSignInGoogleState>(
+      listener: (context, state) {
+        if (state is WellcomeSignInGoogleCubitSuccess) {
+          showSnackBarMethod(context, state.successMessage);
+        }
+      },
+      builder: (context, state) {
+        return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CustomCircleAvatar(
+                color: kWhiteColor,
+                circleImage: 'assets/images/Icon - Google.svg',
+                onTap: () {},
+                isLoading:
+                    state is WellcomeSignInGoogleCubitLoading ? true : false,
+              ),
+              CustomCircleAvatar(
+                color: kSoft,
+                circleImage: 'assets/images/Icon - Apple.svg',
+                onTap: () {},
+                isLoading:
+                    state is WellcomeSignInGoogleCubitLoading ? true : false,
+              ),
+              CustomCircleAvatar(
+                color: Colors.blue.withOpacity(0.6),
+                circleImage: 'assets/images/Icon - Facebook.svg',
+                onTap: () {},
+                isLoading:
+                    state is WellcomeSignInGoogleCubitLoading ? true : false,
+              ),
+            ]);
+      },
+    );
   }
 }
