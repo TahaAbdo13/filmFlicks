6d5af0494c0526b018c9a577f14faa7c42375a55
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-09-03 00:15:47 +0300

    create wellcome sign in with google cubit state

--- lib/features/registeraion/presentation/manager/wellcome_signIn_facebook/wellcome_sign_in_facebook_state.dart
+++ lib/features/registeraion/presentation/manager/wellcome_signIn_facebook/wellcome_sign_in_facebook_state.dart
@@ -4,3 +4,17 @@ part of 'wellcome_sign_in_facebook_cubit.dart';
 sealed class WellcomeSignInFacebookState {}
 
 final class WellcomeSignInFacebookInitial extends WellcomeSignInFacebookState {}
+final class WellcomeSignInFacebookCubitSuccess extends WellcomeSignInFacebookState {
+  final String successMessage;
+
+  WellcomeSignInFacebookCubitSuccess({required this.successMessage});
+}
+
+final class WellcomeSignInFacebookCubitLoading extends WellcomeSignInFacebookState {}
+
+final class WellcomeSignInFacebookCubitFailure extends WellcomeSignInFacebookState {
+  final String errMessage;
+
+  WellcomeSignInFacebookCubitFailure({required this.errMessage});
+}
+
