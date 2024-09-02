a9c1b339b8ab0ca1bfda8fe76f648ed6ff633afd
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-09-02 16:23:25 +0300

    create Wellcome SignIn Google Cubit method

--- lib/features/registeraion/presentation/manager/wellcome_sigIn_google/wellcome_sign_in_google_cubit.dart
+++ lib/features/registeraion/presentation/manager/wellcome_sigIn_google/wellcome_sign_in_google_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
-
-
+import 'package:filmflicks/features/registeraion/data/repository/wellcome_repoImple.dart';
 part 'wellcome_sign_in_google_state.dart';
 
 class WellcomeSignInGoogleCubit extends Cubit<WellcomeSignInGoogleState> {
-  WellcomeSignInGoogleCubit() : super(WellcomeSignInGoogleInitial());
-  
+  final WellcomeRepoimple wellcomeRepoimple;
+  WellcomeSignInGoogleCubit(this.wellcomeRepoimple)
+      : super(WellcomeSignInGoogleInitial());
+
+  Future signInWithGoogle() async {
+    emit(WellcomeSignInGoogleCubitLoading());
+    var result = await wellcomeRepoimple.signInWithGoogle();
+    result.fold((failure) {
+      emit(WellcomeSignInGoogleCubitFailure(errMessage: failure.errMessage));
+    }, (success) {
+      emit(WellcomeSignInGoogleCubitSuccess(
+          successMessage: success.successMessage));
+    });
+  }
 }
