5fa4e742751cf744e0b9bf4c28ba3af28396c2c7
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-09-03 14:16:51 +0300

    create Home Feature and sepertae it to 2 layer

--- lib/features/button_nav_bar/presentation/views/button_nav_bar_view.dart
+++ lib/features/button_nav_bar/presentation/views/button_nav_bar_view.dart
@@ -2,7 +2,6 @@ import 'package:filmflicks/core/utils/widgets/custom_bottom_navigation_bar.dart'
 import 'package:filmflicks/features/button_nav_bar/presentation/manager/botton_navigation_bar/botton_navigation_bar_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 class ButtonNavBarView extends StatelessWidget {
   const ButtonNavBarView({super.key});
   @override
