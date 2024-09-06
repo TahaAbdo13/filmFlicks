fdb809be483f2c81a7f232ed251701c55536f6de
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-09-07 01:51:31 +0300

    create authentication feature

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -2,16 +2,9 @@
 import 'package:filmflicks/features/button_nav_bar/presentation/views/button_nav_bar_view.dart';
 import 'package:filmflicks/features/onBoardering_view/presentaion/views/onBoardring_view.dart';
 import 'package:filmflicks/features/splash/presentaton/views/splash_view.dart';
-
-
-
-
 import 'package:go_router/go_router.dart';
-
 abstract class AppRouter {
   static const kOnBoarderingScreenView = "/OnBoardringScreenView";
-
-
   static const kBottomNaiagationBarview = "/BottomNaiagationBarview";
  
 
