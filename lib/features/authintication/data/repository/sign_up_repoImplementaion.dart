dd3b0f5bdf3e024dd5edd3d5b3c248ec586a1341
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-09-07 16:44:20 +0300

    create sign_up_Cubit

--- lib/features/authintication/data/repository/sign_up_repoImplementaion.dart
+++ lib/features/authintication/data/repository/sign_up_repoImplementaion.dart
@@ -4,10 +4,8 @@ import 'package:filmflicks/core/errors/failure.dart';
 import 'package:filmflicks/core/utils/services/api_services.dart';
 import 'package:filmflicks/features/authintication/data/models/request_token_model.dart';
 import 'package:filmflicks/features/authintication/data/repository/sign_up_repo_interface.dart';
-
 class SignUpRepoimplementaion implements SignUpRepoInterface {
   final ApiServices apiServices;
-
   SignUpRepoimplementaion({required this.apiServices});
   @override
   Future<Either<Failure, RequestTokenModel>> requestokenMehtod() async {
