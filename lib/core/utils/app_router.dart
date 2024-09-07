ddfeb52452364afd7c557474d78d7db337a230b5
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-09-08 02:50:07 +0300

    "Updated AndroidManifest.xml and app_router.dart files"

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -3,10 +3,12 @@ import 'package:filmflicks/core/utils/services/api_services.dart';
 import 'package:filmflicks/features/authintication/data/repository/sign_up_repoImplementaion.dart';
 import 'package:filmflicks/features/authintication/presentation/manager/sign_up_cubit/sign_up_cubit.dart';
 import 'package:filmflicks/features/authintication/presentation/views/sign_in_view.dart';
+import 'package:filmflicks/features/button_nav_bar/presentation/manager/botton_navigation_bar/botton_navigation_bar_cubit.dart';
 import 'package:filmflicks/features/button_nav_bar/presentation/views/button_nav_bar_view.dart';
 import 'package:filmflicks/features/onBoardering_view/presentaion/views/onBoardring_view.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+
 abstract class AppRouter {
   static const kOnBoarderingScreenView = "/OnBoardringScreenView";
   static const kBottomNaiagationBarview = "/BottomNaiagationBarview";
@@ -14,9 +16,11 @@ abstract class AppRouter {
 
   static final GoRouter router = GoRouter(routes: [
     GoRoute(
-      path: "/",
-      builder: (context, state) => const ButtonNavBarView()
-    ),
+        path: "/",
+        builder: (context, state) => BlocProvider(
+              create: (context) => BottonNavigationBarCubit(),
+              child: const ButtonNavBarView(),
+            )),
     GoRoute(
       path: kOnBoarderingScreenView,
       builder: (context, state) => const OnboardringView(),
@@ -25,11 +29,11 @@ abstract class AppRouter {
       path: kBottomNaiagationBarview,
       builder: (context, state) => const ButtonNavBarView(),
     ),
-   
     GoRoute(
       path: kSignInView,
       builder: (context, state) => BlocProvider(
-        create: (context) => SignUpCubit(SignUpRepoimplementaion(apiServices: ApiServices(dio: Dio()))),
+        create: (context) => SignUpCubit(
+            SignUpRepoimplementaion(apiServices: ApiServices(dio: Dio()))),
         child: const SignInView(),
       ),
     ),
