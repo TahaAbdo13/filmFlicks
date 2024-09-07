afff643d7c64f8e8bcc9a176e33e9b168e8b3764
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-09-07 16:51:05 +0300

    create SignUp cubit method

--- lib/features/authintication/presentation/manager/sign_up_cubit/sign_up_cubit.dart
+++ lib/features/authintication/presentation/manager/sign_up_cubit/sign_up_cubit.dart
@@ -1,9 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:filmflicks/features/authintication/data/models/request_token_model.dart';
-
+import 'package:filmflicks/features/authintication/data/repository/sign_up_repo_interface.dart';
 
 part 'sign_up_state.dart';
 
 class SignUpCubit extends Cubit<SignUpState> {
-  SignUpCubit() : super(SignUpInitial());
+  final SignUpRepoInterface signUpRepoInterface;
+  SignUpCubit(this.signUpRepoInterface) : super(SignUpInitial());
+  Future signUp() async {
+    emit(SignUpCubitLoading());
+    var result = await signUpRepoInterface.requestokenMehtod();
+    result.fold((failure) {
+      emit(SignUpCubitFailure(errMessage: failure.errMessage));
+    }, (requestTokenModel) {
+      emit(SignUpCubitSuccess(requestTokenModel: requestTokenModel));
+    });
+  }
 }
