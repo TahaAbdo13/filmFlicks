fb0a25659097e418cf266f0247413c09670c7472
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-09-07 13:32:37 +0300

    add headers to api get sevices

--- lib/core/utils/services/api_services.dart
+++ lib/core/utils/services/api_services.dart
@@ -4,7 +4,13 @@ class ApiServices {
   final Dio dio;
 
   ApiServices({required this.dio});
-  Future<dynamic> get({required String url}) async {
+  Future<dynamic> get({required String url , String? authorization}) async {
+    if(authorization!=null){
+      dio.options.headers = {
+        'Authorization':"Bearer $authorization",
+      };
+    }
+    
     var request = await dio.get(url);
     Map<String, dynamic> requestData = request.data;
     return requestData;
